9a56dda84521de1ec15f5fd6021c598da5048462
mcferrenm <mcferrenmax@gmail.com>
2019-02-19 15:34:23 -0500

    Add all zoo API routes and calls to db

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
+const express = require("express");
+const helmet = require("helmet");
+
+const knex = require("knex");
 
 const knexConfig = {
-  client: 'sqlite3',
-  connect: {
-    filename: './data/lambda.sqlite3'
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
   },
   useNullAsDefault: true
-}
+};
 
 const db = knex(knexConfig);
 
@@ -18,6 +19,80 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const zoos = await db("zoos");
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json({ error: "Error while getting zoos" });
+  }
+});
+
+server.get("/api/zoos/:id", async (req, res) => {
+  try {
+    const zoo = await db("zoos")
+      .where({ id: req.params.id })
+      .first();
+    if (zoo) {
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ error: "Zoo not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error while getting zoo" });
+  }
+});
+
+server.post("/api/zoos", async (req, res) => {
+  try {
+    if (!req.body.name) {
+      res.status(400).json({ error: "Please provide a name for new zoo." });
+    } else {
+      const [id] = await db("zoos").insert(req.body);
+      res.status(201).json(id);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error while inserting zoo" });
+  }
+});
+
+server.delete("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id })
+      .del();
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: "Zoo not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error while deleting zoo" });
+  }
+});
+
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    if (!req.body.name) {
+      res.status(400).json({ error: "Please provide a name for new zoo." });
+    } else {
+      const count = await db("zoos")
+        .where({ id: req.params.id })
+        .update(req.body);
+      if (count > 0) {
+        const zoo = await db("zoos")
+          .where({ id: req.params.id })
+          .first();
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ error: "Zoo not found" });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error while deleting zoo" });
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
